d1568c628e31deb76834b88a0a2d97e075b84f4a
toshinori sato <tyahhahha@gmail.com>
2020-03-17 21:12:02 +0900

    refactoring result mode view

--- src/component/view/ResultModeView.tsx
+++ src/component/view/ResultModeView.tsx
@@ -2,80 +2,47 @@ import React from "react";
 import styled from "@emotion/styled";
 import { css } from "emotion";
 
-import { formatTime } from "../../model/time";
-import { diffTime } from "../../model/time";
+import { createResult, ResultItem } from "../../model/result";
 import { useTypingContext } from "../../hooks/useTypingContext";
 
 export const ResultModeView = () => {
   const { problems, startTime, endTime, misses } = useTypingContext();
-
-  const missStrings = [...misses]
-    .sort((a, b) => b[1] - a[1])
-    .slice(0, 3)
-    .map(([s, num]) => `${s}(${num}回)`);
-
-  const allInputKeyCount = problems.reduce(
-    (acc, c) => acc + c.alphabet.length,
-    0
-  );
-
-  const allMissTypeCount = Array.from(misses.values()).reduce(
-    (acc, c) => acc + c,
-    0
-  );
+  const resultItems = createResult(startTime, endTime, problems, misses);
 
   return (
-    <div
-      className={css`
-        width: 100%;
-        height: 100%;
-      `}
-    >
-      <div
-        className={css`
-          margin: 5px;
-          height: calc(100% - 20px);
-          padding: 0;
-          border-color: rgb(55, 55, 65);
-          border-style: solid;
-          border-width: 1px;
-          background-color: #c1bdeb;
-        `}
-      >
-        <ResultItemList
-          items={[
-            ["入力時間", formatTime(diffTime(startTime, endTime))],
-            ["入力キー数", `${allInputKeyCount}回`],
-            ["総ミスタイプ数", `${allMissTypeCount}回`],
-            [
-              "WPM",
-              `${Math.floor(
-                (allInputKeyCount / (endTime - startTime)) * (1000 * 60)
-              )}`
-            ],
-            [
-              "正誤率",
-              `${Math.floor(
-                (allInputKeyCount / (allInputKeyCount + allMissTypeCount)) * 100
-              )}%`
-            ],
-            ["ミスしやすいキー", missStrings.join(", ")]
-          ]}
-        />
-        <p
-          className={css`
-            margin: auto;
-            text-align: center;
-          `}
-        >
-          (Rキーでもう一回)
-        </p>
-      </div>
-    </div>
+    <ResultFrame>
+      <ResultContentArea>
+        <CenterizeResult>
+          <ResultItemList items={resultItems} />
+          <CenterizedSimpleLabel>(Rキーでもう一回)</CenterizedSimpleLabel>
+        </CenterizeResult>
+      </ResultContentArea>
+    </ResultFrame>
   );
 };
 
-const ResultItemList = (props: { items: [string, string][] }) => (
+const ResultFrame = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const ResultContentArea = styled.div`
+  margin: 5px;
+  height: calc(100% - 20px);
+  padding: 0;
+  border-color: rgb(55, 55, 65);
+  border-style: solid;
+  border-width: 1px;
+  background-color: #c1bdeb;
+`;
+
+const CenterizeResult = styled.div`
+  position: relative;
+  top: 40%;
+  transform: translateY(-50%);
+`;
+
+const ResultItemList = (props: { items: ResultItem[] }) => (
   <ul
     className={css`
       width: 60%;
@@ -84,28 +51,28 @@ const ResultItemList = (props: { items: [string, string][] }) => (
     `}
   >
     {props.items.map((item, index) => (
-      <ResultItem key={index} name={item[0]} value={item[1]} />
+      <ResultItemView key={index} item={item} />
     ))}
   </ul>
 );
 
-const ResultItem = (props: { name: string; value: string }) => (
+const ResultItemView = (props: { item: ResultItem }) => (
   <li
     className={css`
       list-style: none;
-      border-width: 1px;
-      border-bottom: solid;
+      border-bottom: 1px solid #777;
+      display: flex;
     `}
   >
-    <ResultItemData>{props.name}</ResultItemData>
+    <ResultItemData>{props.item[0]}</ResultItemData>
     <ResultItemData
       className={css`
         text-align: center;
-        width: 15em;
         font-weight: bold;
+        flex-grow: 1;
       `}
     >
-      {props.value}
+      {props.item[1]}
     </ResultItemData>
   </li>
 );
@@ -114,4 +81,9 @@ const ResultItemData = styled.div`
   margin: 10px 0 0 10px;
   display: inline-block;
   width: 10em;
+`;
+
+const CenterizedSimpleLabel = styled.p`
+  margin: auto;
+  text-align: center;
 `;
---
+++ src/model/result.ts
@@ -0,0 +1,43 @@
+import { Problem } from "./problem";
+import { formatTime, diffTime } from "./time";
+
+export type ResultItem = [string, string];
+
+export const createResult = (
+  startTime: number,
+  endTime: number,
+  problems: Problem[],
+  misses: Map<string, number>
+): ResultItem[] => {
+  const missStrings = [...misses]
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 3)
+    .map(([s, num]) => `${s}(${num}回)`);
+
+  const allInputKeyCount = problems.reduce(
+    (acc, c) => acc + c.alphabet.length,
+    0
+  );
+
+  const allMissTypeCount = Array.from(misses.values()).reduce(
+    (acc, c) => acc + c,
+    0
+  );
+
+  return [
+    ["入力時間", formatTime(diffTime(startTime, endTime))],
+    ["入力キー数", `${allInputKeyCount}回`],
+    ["総ミスタイプ数", `${allMissTypeCount}回`],
+    [
+      "WPM",
+      `${Math.floor((allInputKeyCount / (endTime - startTime)) * (1000 * 60))}`
+    ],
+    [
+      "正誤率",
+      `${Math.floor(
+        (allInputKeyCount / (allInputKeyCount + allMissTypeCount)) * 100
+      )}%`
+    ],
+    ["ミスしやすいキー", missStrings.join(", ")]
+  ];
+};
