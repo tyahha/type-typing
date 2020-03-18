cb9181194bcd4e272f64354c19943d007ae722a3
toshinori sato <tyahhahha@gmail.com>
2020-03-19 01:00:54 +0900

    add jaUnit

---
+++ src/model/jaUnit.ts
@@ -0,0 +1,329 @@
+interface JaInput {
+  kana: string;
+  keys: string[];
+}
+
+export const SingleJaInputs: JaInput[] = [
+  {
+    kana: "あ",
+    keys: ["A"]
+  },
+  {
+    kana: "い",
+    keys: ["I"]
+  },
+  {
+    kana: "う",
+    keys: ["U"]
+  },
+  {
+    kana: "え",
+    keys: ["E"]
+  },
+  {
+    kana: "か",
+    keys: ["KA", "CA"]
+  },
+  {
+    kana: "き",
+    keys: ["KI"]
+  },
+  {
+    kana: "く",
+    keys: ["KU", "CU"]
+  },
+  {
+    kana: "け",
+    keys: ["KE"]
+  },
+  {
+    kana: "こ",
+    keys: ["KO", "CO"]
+  },
+  {
+    kana: "さ",
+    keys: ["SA"]
+  },
+  {
+    kana: "し",
+    keys: ["SI", "SHI", "CI"]
+  },
+  {
+    kana: "す",
+    keys: ["SU"]
+  },
+  {
+    kana: "せ",
+    keys: ["SE", "CE"]
+  },
+  {
+    kana: "そ",
+    keys: ["SO"]
+  },
+  {
+    kana: "た",
+    keys: ["TA"]
+  },
+  {
+    kana: "ち",
+    keys: ["TI", "CHI"]
+  },
+  {
+    kana: "つ",
+    keys: ["TU", "TSU"]
+  },
+  {
+    kana: "て",
+    keys: ["TE"]
+  },
+  {
+    kana: "と",
+    keys: ["TO"]
+  },
+  {
+    kana: "な",
+    keys: ["NA"]
+  },
+  {
+    kana: "に",
+    keys: ["NI"]
+  },
+  {
+    kana: "ぬ",
+    keys: ["NU"]
+  },
+  {
+    kana: "ね",
+    keys: ["NE"]
+  },
+  {
+    kana: "の",
+    keys: ["NO"]
+  },
+  {
+    kana: "は",
+    keys: ["HA"]
+  },
+  {
+    kana: "ひ",
+    keys: ["HI"]
+  },
+  {
+    kana: "ふ",
+    keys: ["FU", "HU"]
+  },
+  {
+    kana: "へ",
+    keys: ["HE"]
+  },
+  {
+    kana: "ほ",
+    keys: ["HO"]
+  },
+  {
+    kana: "ま",
+    keys: ["MA"]
+  },
+  {
+    kana: "み",
+    keys: ["MI"]
+  },
+  {
+    kana: "む",
+    keys: ["MU"]
+  },
+  {
+    kana: "め",
+    keys: ["ME"]
+  },
+  {
+    kana: "も",
+    keys: ["MO"]
+  },
+  {
+    kana: "や",
+    keys: ["YA"]
+  },
+  {
+    kana: "ゆ",
+    keys: ["YU"]
+  },
+  {
+    kana: "よ",
+    keys: ["YO"]
+  },
+  {
+    kana: "わ",
+    keys: ["WA"]
+  },
+  {
+    kana: "を",
+    keys: ["WO"]
+  },
+  {
+    kana: "ん",
+    keys: ["NN"]
+    // Nを2回押さなくてもいいルールをどうするか
+  },
+  {
+    kana: "が",
+    keys: ["GA"]
+  },
+  {
+    kana: "ぎ",
+    keys: ["GI"]
+  },
+  {
+    kana: "ぐ",
+    keys: ["GU"]
+  },
+  {
+    kana: "げ",
+    keys: ["GE"]
+  },
+  {
+    kana: "ご",
+    keys: ["GO"]
+  },
+  {
+    kana: "ざ",
+    keys: ["ZA"]
+  },
+  {
+    kana: "じ",
+    keys: ["JI", "ZI"]
+  },
+  {
+    kana: "ず",
+    keys: ["ZU"]
+  },
+  {
+    kana: "ぜ",
+    keys: ["ZE"]
+  },
+  {
+    kana: "ぞ",
+    keys: ["ZO"]
+  },
+  {
+    kana: "だ",
+    keys: ["DA"]
+  },
+  {
+    kana: "ぢ",
+    keys: ["DI"]
+  },
+  {
+    kana: "づ",
+    keys: ["DU"]
+  },
+  {
+    kana: "で",
+    keys: ["DE"]
+  },
+  {
+    kana: "ど",
+    keys: ["DO"]
+  },
+  {
+    kana: "ば",
+    keys: ["BA"]
+  },
+  {
+    kana: "び",
+    keys: ["BI"]
+  },
+  {
+    kana: "ぶ",
+    keys: ["BU"]
+  },
+  {
+    kana: "べ",
+    keys: ["BE"]
+  },
+  {
+    kana: "ぼ",
+    keys: ["BO"]
+  },
+  {
+    kana: "ぱ",
+    keys: ["PA"]
+  },
+  {
+    kana: "ぴ",
+    keys: ["PI"]
+  },
+  {
+    kana: "ぷ",
+    keys: ["PU"]
+  },
+  {
+    kana: "ぺ",
+    keys: ["PE"]
+  },
+  {
+    kana: "ぽ",
+    keys: ["PO"]
+  },
+  {
+    kana: "ゑ",
+    keys: ["WYE"]
+  },
+  {
+    kana: "ゐ",
+    keys: ["WYI"]
+  },
+  {
+    kana: "ゐ",
+    keys: ["WYI"]
+  },
+  {
+    kana: "ー",
+    keys: ["-"]
+  },
+  {
+    kana: "ヴ",
+    keys: ["VU"]
+  },
+  // 小さな文字
+  {
+    kana: "ぁ",
+    keys: ["LA", "XA"]
+  },
+  {
+    kana: "ぃ",
+    keys: ["LI", "XI"]
+  },
+  {
+    kana: "ぅ",
+    keys: ["LU", "XU"]
+  },
+  {
+    kana: "ぇ",
+    keys: ["LE", "XE"]
+  },
+  {
+    kana: "ぉ",
+    keys: ["LO", "XO"]
+  },
+  {
+    kana: "っ",
+    keys: ["LTU", "XTU", "LTSU", "XTSU"]
+  },
+  {
+    kana: "ゃ",
+    keys: ["LYA", "XYA"]
+  },
+  {
+    kana: "ゅ",
+    keys: ["LYU", "XYU"]
+  },
+  {
+    kana: "ゎ",
+    keys: ["LWA", "XWA"]
+  },
+  {
+    kana: "ぃ",
+    keys: ["LI", "XI"]
+  }
+];
